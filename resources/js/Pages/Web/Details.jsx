8e533887ceca14a8ca7c138ae3251aa09034973a
Gazi-Nasim <gazinasim001@gmail.com>
2025-07-28 13:48:08 +0600

    ci cd test 2

--- resources/js/Pages/Web/Details.jsx
+++ resources/js/Pages/Web/Details.jsx
@@ -56,10 +56,9 @@ function Details() {
                                     <li>{job.educations?.[0]?.higherSecondary || ''}</li>
                                     <li>{job.educations?.[0]?.graduation || ''}</li>
                                 </ul>
-
                             </div>
-
                         </div>
+
                         <div className="flex justify-end">
                             <div className="flex justify-end mr-2">
                                 <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded align-end" onClick={() => { window.location.href = '/' }}>See all Jobs</button>
@@ -68,62 +67,8 @@ function Details() {
                                 <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded align-end" onClick={() => { window.location.href = `/apply/${job.id}` }}>Apply Now</button>
                             </div>
                         </div>
-
-                        {/* <div className="flex col-sm-12">
-                                    <div className="row featured-wrap" onClick={() => goToJobDetails('1389524')} style={{ height: '157px' }}>
-
-                                        <div className="col-sm-8 details">
-                                            <div className="title">
-                                                <a onClick={() => goToJobDetails('1389524')} target="_blank" href="https://jobs.bdjobs.com/jobdetails/?id=1389524&amp;ln=1">
-                                                    Student Consultants
-                                                </a>
-                                            </div>
-                                            <div className="company">
-                                                {element.name}
-                                                Universal Study Agency
-                                            </div>
-
-                                            <div className="loccal">
-                                                <p>
-                                                    <img className="img-responsive" src="//corporate.bdjobs.com/logos/37122_3.png" alt="logo of Universal Study Agency" />
-                                                </p>
-                                            </div>
-
-                                        </div>
-
-                                        <div className="col-sm-4 logo">
-                                            <img className="img-responsive" src="//corporate.bdjobs.com/logos/37122_3.png" alt="logo of Universal Study Agency" />
-                                        </div>
-
-                                        <div className="col-sm-12 details">
-                                            <div className="exp loccal" style={{ margintop: "5px" }}>
-                                                <p>
-                                                    <img src="" alt="Experience required" />
-                                                    2 to 3 year(s)
-                                                </p>
-                                                <p className="dt">
-                                                    <img src="" alt="Deadline for apply the job" />
-                                                    <strong>25 Aug</strong> 2025
-                                                </p>
-                                            </div>
-                                        </div>
-
-                                        <div className="col-sm-12 details">
-                                            <div className="requires hide">
-                                                <div className="promo-text">
-                                                    <ul style={{ margin: '5px 0 0 0' }}>
-                                                        <li></li>
-                                                    </ul>
-                                                </div>
-
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <img src="" className="down-arrow" alt="" />
-                                </div> */}
                     </div>
                 </div>
-
             </div>
         </div>
 
--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'verified',]], funct
 });
 
 
-Route::middleware('auth')->group(function (f) {
+Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
